96027018ac60018ffecfcbc647f95dad34a16479
PiotrPrezydent <piotrprezydent33@gmail.com>
2026-03-30 15:50:31 +0200

    old application layer cleanup

--- frontend/old-modules/android/src/main/java/pl/edu/ur/blokur/ui/android/AndroidApplication.kt
+++
@@ -1,8 +0,0 @@
-package pl.edu.ur.blokur.ui.android
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-// main application class, this is app main process, initialize all that is needed here
-@HiltAndroidApp
-class AndroidApplication : Application()
