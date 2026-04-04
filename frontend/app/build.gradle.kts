a535ed13904eb60b57b5d4b207960873be613ed0
Piotrek Prezydent <piotrprezydent33@gmail.com>
2026-04-04 21:24:40 +0200

    reverted modules name after dev update

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -2,14 +2,17 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    alias(libs.plugins.hilt.android)
+    kotlin("kapt")
 }
 
 android {
-    namespace = "pl.edu.ur.blokur.android"
+    namespace = "pl.edu.ur.blockur"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "pl.edu.ur.blokur.android"
+        applicationId = "pl.edu.ur.blockur"
         minSdk = 27
         targetSdk = 36
         versionCode = 1
@@ -27,16 +30,13 @@ android {
             )
         }
     }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-
     kotlinOptions {
         jvmTarget = "11"
     }
-
     buildFeatures {
         compose = true
     }
@@ -46,29 +46,24 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-
-    implementation("androidx.navigation:navigation-compose:2.9.0")
-    implementation("androidx.compose.material:material-icons-extended")
-
-    implementation(libs.retrofit)
-    implementation(libs.retrofit.converter.gson)
-    implementation(libs.okhttp.logging)
-    implementation(libs.androidx.datastore.preferences)
-    implementation(libs.androidx.lifecycle.viewmodel.compose)
-
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+
+    implementation(libs.hilt.navigation.compose)
+    implementation(project(":presentation"))
+    implementation(project(":domain"))
+    implementation(project(":infrastructure"))
 }
--- frontend/settings.gradle.kts
+++ frontend/settings.gradle.kts
@@ -20,9 +20,9 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "My Application"
-include(":app")
+include(":app-old")
 include(":domain")
 include(":application")
 include(":infrastructure")
 include(":presentation")
-include(":app-refactored")
+include(":app")
