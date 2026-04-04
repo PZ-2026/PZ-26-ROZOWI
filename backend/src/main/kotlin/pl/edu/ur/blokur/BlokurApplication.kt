e4c99b21b1f3a2022e059a0d2432bf259fe3e6cc
hubertfus <hf131432@stud.ur.edu.pl>
2026-04-04 17:29:09 +0200

    [HOTFIX] usunięcie FlywayConfig blokującego auto-konfigurację bazy

--- backend/src/main/kotlin/pl/edu/ur/blokur/BlokurApplication.kt
+++ backend/src/main/kotlin/pl/edu/ur/blokur/BlokurApplication.kt
@@ -3,12 +3,7 @@ package pl.edu.ur.blokur
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(
-    excludeName = [
-        "org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration",
-        "org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration",
-    ],
-)
+@SpringBootApplication
 class BlokurApplication
 
 fun main(args: Array<String>) {
